e7655aa596e06b0959cc9003baea9bbebe2c001b
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2016-06-17 06:21:41 +0200

    cleanup

--- prepare-textures.bat
+++ prepare-textures.bat
@@ -1,35 +1,32 @@
 @echo off
 
-if not exist "textures" (
-  echo "textures" directory not found!
-  pause
-  exit 1
-)
-
-for /f "tokens=1-6 delims=," %%a in (texalias.txt) do (
-  if not exist "textures\%%a.dds" (
-    copy /y "textures\%%b.dds" "textures\%%a.dds" 1>nul
-    if not "%%c" == "" (
-      copy /y "textures\%%b.dds" "textures\%%c.dds" 1>nul
-    )
-  ) else (
-    if not "%%b" == "" (
-      copy /y "textures\%%a.dds" "textures\%%b.dds" 1>nul
-    )
-    if not "%%c" == "" (
-      copy /y "textures\%%a.dds" "textures\%%c.dds" 1>nul
-    )
-    if not "%%d" == "" (
-      copy /y "textures\%%a.dds" "textures\%%d.dds" 1>nul
-    )
-    if not "%%e" == "" (
-      copy /y "textures\%%a.dds" "textures\%%e.dds" 1>nul
-    )
-    if not "%%f" == "" (
-      copy /y "textures\%%a.dds" "textures\%%f.dds" 1>nul
+if exist "textures" (
+  for /f "tokens=1-6 delims=," %%a in (texalias.txt) do (
+    if not exist "textures\%%a.dds" (
+      copy /y "textures\%%b.dds" "textures\%%a.dds" 1>nul
+      if not "%%c" == "" (
+        copy /y "textures\%%b.dds" "textures\%%c.dds" 1>nul
+      )
+    ) else (
+      if not "%%b" == "" (
+        copy /y "textures\%%a.dds" "textures\%%b.dds" 1>nul
+      )
+      if not "%%c" == "" (
+        copy /y "textures\%%a.dds" "textures\%%c.dds" 1>nul
+      )
+      if not "%%d" == "" (
+        copy /y "textures\%%a.dds" "textures\%%d.dds" 1>nul
+      )
+      if not "%%e" == "" (
+        copy /y "textures\%%a.dds" "textures\%%e.dds" 1>nul
+      )
+      if not "%%f" == "" (
+        copy /y "textures\%%a.dds" "textures\%%f.dds" 1>nul
+      )
     )
   )
+  echo "done"
+) else (
+  echo "textures" directory not found!
 )
-
-echo "done"
 pause
